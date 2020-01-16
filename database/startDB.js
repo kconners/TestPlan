508e99844d20f49675dfcd4751a60f25e257ff44
kconners <kdcntexas@gmail.com>
2020-01-16 17:36:50 -0600

    commiting changes....

--- database/startDB.js
+++ database/startDB.js
@@ -4,16 +4,16 @@ const mariadb = require('mariadb');
 const dbName = 'client_data';
 
 module.exports.pool = mariadb.createPool({
-    //host     : '192.168.99.100',
-    host     : '127.0.0.1',
+    host     : '192.168.99.100',
+    //host     : '127.0.0.1',
     port     : '3355',
     user     : 'root',
     password : 'Password1*',
     connectionLimit: 5
 });
 module.exports.pooldb = mariadb.createPool({
-    //host     : '192.168.99.100',
-    host     : '127.0.0.1',
+    host     : '192.168.99.100',
+    //host     : '127.0.0.1',
     port     : '3355',
     user     : 'root',
     password : 'Password1*',
